63ae052c0e77acd35271383ffd4b9c8ba56cb28d
wangxiui <1036900394@qq.com>
2021-10-29 07:23:59 +0800

    feat: 实现了表单页面开发，可以和网页正常通信。

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="popup pa16 box-shadow bg-fff tal flex flex-column"
+  <div class="popup ma16 shadow-primary bg-fff tal flex flex-column"
     :style="[styleObj]">
-    <div class="flex-grow">
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="抢购时间">
+    <div class="flex-grow pa16">
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="抢购时间" prop="deadline">
           <el-date-picker
             size="mini"
             v-model="form.deadline"
@@ -12,7 +12,10 @@
             placeholder="请选择时间">
           </el-date-picker>
         </el-form-item>
-        <el-form-item label="触发频率">
+        <el-form-item label="抢购数量" prop="count">
+          <el-input-number v-model="form.count" :min="0" :step="10"></el-input-number>
+        </el-form-item>
+        <el-form-item label="触发频率" prop="frequency">
           <span>每秒</span>
           <el-input-number class="input-suffix_percentage tal" data-unit="次" v-model="form.frequency" :controls="false" :min="1" :max="20" :step="1" :precision="0" placeholder="0~20"></el-input-number>
         </el-form-item>
@@ -35,138 +38,142 @@
 
 <script>
 import {formatCountdown} from '../utils'
-  export default {
-    name: "App",
-    data() {
-      return {
-        styleObj: {minWidth: '500px', minHeight: '500px'},
-        form: {
-          // 抢购时间
-          deadline: '',
-          // 触发频次
-          frequency: 1,
-        },
-        // 触发频次列表
-        frequencyList: [5, 10, 15, 20, 25, 30],
-        // 倒计时
-        countDown: '',
-        // 定时器
-        countdownId: '',
-        config: {
-          format: 'HH:mm:ss'
-        }
-      }
-    },
-    created() {
-      this.init()
-      this.getBtn()
-    },
-    activated() {
-      this.init();
-    },
-    beforeDestroy() {
-      this.stopTimer();
-    },
-    methods: {
-      /**
-       * 初始化
-       */
-      init() {
-        const {deadline} = this.form
-        const now = new Date().getTime()
-        if (deadline >= now) {
-          this.startTimer()
-          return
-        }
-        this.stopTimer()
+export default {
+  name: "App",
+  data() {
+    return {
+      styleObj: {width: '400px'},
+      form: {
+        // 抢购时间
+        deadline: '',
+        // 抢购数量
+        count: 0,
+        // 触发频次
+        frequency: 1,
       },
-      getBtn() {
-        // chrome.tabs.query可以通过回调函数获得当前页面的信息tabs
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-          console.log('tabs11', tabs);
+      // 倒计时
+      countDown: '',
+      // 定时器
+      countdownId: '',
+      config: {
+        format: 'HH:mm:ss'
+      },
+      rules: {
+        deadline: [{ required: true, trigger: "blur", message: "请选择抢购时间。" }],
+        count: [{ required: true, trigger: "blur", message: "请输入抢购数量。" }],
+        frequency: [{ required: true, trigger: "blur", message: "请输入触发频次。" }],
+      },
+      win: {}
+    }
+  },
+  created() {
+    this.init()
+  },
+  activated() {
+    this.init();
+  },
+  beforeDestroy() {
+    this.stopTimer();
+  },
+  methods: {
+    /**
+     * 初始化
+     */
+    init() {
+      const {deadline} = this.form
+      const now = new Date().getTime()
+      if (deadline >= now) {
+        this.startTimer()
+        return
+      }
+      this.stopTimer()
+    },
+    /**
+     * 开始抢购
+     */
+    rushing() {
+      // chrome.tabs.query可以通过回调函数获得当前页面的信息tabs
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        console.log('tabs11', tabs);
+        let tabId = tabs.length ? tabs[0].id : null;
 
-          let tabId = tabs.length ? tabs[0].id : null;
-          // 向当前页面注入 JavaScript 脚本
-          chrome.tabs.executeScript(tabId || null, {
-            file: 'popup/content.js'
-          }, function () {
-            // 向目标网页进行通信，向 recommend.js 发送一个消息
-            chrome.tabs.sendMessage(tabId, {
-              message: 'GET_TOPIC_INFO',
-            }, function (response) {
+        // 向当前页面注入 JavaScript 脚本
+        chrome.tabs.executeScript(tabId || null, {
+          file: './popup/content.js'
+        }, function (_) {
+          let e = chrome.runtime.lastError;
+          if (e !== undefined) {
+            console.log(tabId, _, e);
+          }
+
+          // 向目标网页进行通信，向 content.js 发送一个消息
+          chrome.tabs.sendMessage(tabId, {
+            message: 'GET_TOPIC_INFO',
+            data: this.form
+          }, function (response) {
+            if (!window.chrome.runtime.lastError) {
               console.log('response', response);
+              if (!response) return false;
               // 获取到返回的文章 title 、url、description
               // host.article.title = response.title;
               // host.article.link = response.link;
               // host.article.description = response.description;
-            });
+            } else {
+              console.log('window.chrome.runtime.lastError', window.chrome.runtime.lastError);
+            }
           });
-
-          // 发送一个copy消息出去
-          /*chrome.tabs.sendMessage(tabs[0].id, { action: "copy" }, function (response) {
-            console.log('response11', response);
-            // 这里的回调函数接收到了要抓取的值，获取值得操作在下方content-script.js
-            // 将值存在background.js的data属性里面。
-            var win = chrome.extension.getBackgroundPage();
-            win.data=response;
-            console.log(response);
-
-            // 监听来自content-script的消息
-            chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
-            {
-              console.log('收到来自content-script的消息：');
-              console.log(request, sender, sendResponse);
-              sendResponse('我是后台，我已收到你的消息：' + JSON.stringify(request));
-            });
-          });*/
         });
-      },
-      paste() {
-        var win = chrome.extension.getBackgroundPage();
-        if (win.data) {
-          chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            console.log(tabs);
-            chrome.tabs.sendMessage(tabs[0].id, { action: "paste", data: win.data }, function (response) {
-              console.log(response);
-            });
+      });
+    },
+    async handleSetting() {
+      const valid = await this.$refs.form.validate().catch(err => console.log(err))
+      if (!valid) return false
+      // 将值存在background.js的data属性里面。
+      this.win = chrome.extension.getBackgroundPage();
+      this.win.conditionObj = this.form;
+    },
+    paste() {
+      var win = chrome.extension.getBackgroundPage();
+      if (win.data) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+          console.log(tabs);
+          chrome.tabs.sendMessage(tabs[0].id, { action: "paste", data: win.data }, function (response) {
+            console.log(response);
           });
-        }
-      },
-      async onFinish() {
-        // await this.$nextTick()
-        // const ele = document.getElementsByClassName('user-info-no')[0]
-        // console.log('ele', ele);
-        // ele.onclick({target: ele})
-        // window.dispatchEvent(ele.onclick)
-        // this.$message.success('已结束！')
-      },
-      startTimer() {
-        if (this.countdownId) return;
-        const {deadline} = this.form
-        this.getBtn()
-
-        this.countdownId = window.setInterval(() => {
-
-          if (deadline > Date.now()) {
-            this.countDown = formatCountdown(deadline, this.config)
-            // onChange(deadline - Date.now());
-          }
-          this.init()
-        }, 1000 / this.form.frequency);
-      },
-      stopTimer() {
-        if (this.countdownId) {
-          clearInterval(this.countdownId);
-          this.countdownId = undefined;
-
-          const {deadline} = this.form
-          if (deadline < Date.now()) {
-            this.onFinish();
-          }
-        }
+        });
       }
     },
-  }
+    async onFinish() {
+      this.rushing()
+    },
+    startTimer() {
+      if (this.countdownId) return;
+      const {deadline} = this.form
+      // 保存设置
+      this.handleSetting()
+
+      // 开启倒计时
+      this.countdownId = window.setInterval(() => {
+        if (deadline > Date.now()) {
+          this.countDown = formatCountdown(deadline, this.config)
+          // onChange(deadline - Date.now());
+        }
+        this.init()
+      }, 1000 / this.form.frequency);
+    },
+    stopTimer() {
+      if (this.countdownId) {
+        clearInterval(this.countdownId);
+        this.countdownId = undefined;
+
+        const {deadline} = this.form
+        if (deadline < Date.now()) {
+          this.onFinish();
+        }
+      }
+    }
+  },
+}
 </script>
 
 <style scoped lang="scss">
--- src/popup/content.js
+++ src/popup/content.js
@@ -6,6 +6,9 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
   // sendResponse('我收到你的消息了：'+JSON.stringify("request"));
   // return true
   if(request.message === 'GET_TOPIC_INFO') {
+    console.log('获取 title', request);
+    // editInputValue()
+    // clickBtn()
     // 获取 title
     let title = document.getElementsByTagName('title')[0].textContent;
     let descriptionEl = doc.querySelectorAll('meta[name=description]')[0];
@@ -21,3 +24,25 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
     console.log('request, sender', request, sender);
   }
 });
+
+function editInputValue() {
+  const input = doc.getElementsByClassName('search-input')
+  console.log('input', input);
+  input[0].value = '22222'
+}
+
+/**
+ * 触发按钮点击时事件
+ */
+function clickBtn() {
+  const ele = doc.getElementsByClassName('app-link')
+  console.log('ele', ele);
+
+  // 方案一（成功）
+  // ele[0].click()
+
+  // 方案二（成功）
+  const e = document.createEvent("MouseEvents");
+  e.initEvent("click", true, true);
+  ele[0].dispatchEvent(e);
+}
